c2f64b4ae47cca8ca293efd8aa7d94e7d7d3e87d
Uladzislau Kaminski <lovenikol95@gmail.com>
2020-03-12 19:07:54 +0300

    lol2

---
+++ public/js.js
@@ -0,0 +1,27 @@
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : 1,
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v6.0'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+
+setTimeout(()=>{
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+       });
+},1000)
